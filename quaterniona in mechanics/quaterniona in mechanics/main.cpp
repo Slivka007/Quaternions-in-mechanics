391be557fc87fefa3c460c0c24c757d7986f7465
Gleb Slivka <glebslivka@MacBook-Air-Gleb.local>
2023-11-18 13:54:59 +0300

    Add project

---
+++ quaterniona in mechanics/quaterniona in mechanics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  quaterniona in mechanics
+//
+//  Created by Gleb Slivka on 11/18/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ quaterniona in mechanics/quaterniona in mechanics/qauaternions.hpp
@@ -0,0 +1,13 @@
+//
+//  qauaternions.hpp
+//  quaterniona in mechanics
+//
+//  Created by Gleb Slivka on 11/18/23.
+//
+
+#ifndef qauaternions_hpp
+#define qauaternions_hpp
+
+#include <stdio.h>
+
+#endif /* qauaternions_hpp */
